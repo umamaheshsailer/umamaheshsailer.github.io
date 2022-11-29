90de3604cba201197f7b1cee4b89c0c388112fc2
mahesh <sarakanamumamahesh@gmail.com>
2022-11-29 13:00:16 +0530

    added assets and other sections

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-const brand_logo = document.getElementById("brand");
-console.log(brand_logo);
+let brand_logo = document.getElementsByClassName("brand");
+let skills = document.getElementsByClassName("skills");
+// console.log(brand_logo);
+// console.log(skills.item(0));
 setInterval(() => {
-  // brand_logo.animate();
+  Array.from(skills).forEach((element) => {
+    // console.log(element);
+  });
+  // console.log(brand_logo);
+  if (brand_logo[0].innerHTML == "..." && brand_logo[1].innerHTML == "...") {
+    brand_logo[0].innerHTML = "..";
+    brand_logo[1].innerHTML = "..";
+  } else {
+    brand_logo[0].innerHTML = "...";
+    brand_logo[1].innerHTML = "...";
+  }
 }, 2000);
